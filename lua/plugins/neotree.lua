e4bc1c9e22685021f00bce5612c7f59b40ca9122
maulik13 <maulik.kataria@gmail.com>
2024-06-30 23:20:44 +0200

    update how neotree handles hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,5 +2,22 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {},
+    filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        show_hidden_count = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+          "thumb.db",
+          "thumb",
+          ".idea",
+          "node_modules",
+          ".cache",
+        },
+      },
+    },
   },
 }
