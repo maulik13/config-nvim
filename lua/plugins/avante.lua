179664e966bc114715b9493ab1e6c8999f4cdc76
maulik13 <maulik.kataria@gmail.com>
2024-12-02 15:33:40 +0100

    update avante config and upgrade plugins

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,9 +1,14 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
   },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
