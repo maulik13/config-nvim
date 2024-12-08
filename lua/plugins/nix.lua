05ab3f5e6420d732dbeb265a6f34a9d9a4a2e061
maulik13 <maulik.kataria@gmail.com>
2024-12-08 08:12:17 +0100

    update nix config

---
+++ lua/plugins/nix.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = { "nixfmt" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ft = { "nix" },
+    opts = {},
+    config = function(_, opts)
+      require("lspconfig").nil_ls.setup(opts)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}
